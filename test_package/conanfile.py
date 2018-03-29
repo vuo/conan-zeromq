3c370eb7a6f401c6f2941cbbd477910d3d32e1cb
smokris <smokris@softpixel.com>
2018-03-29 15:54:29 -0400

    Test LC_RPATH.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -8,17 +8,17 @@ class ZeroMQTestConan(ConanFile):
         self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
-        self.copy('*',       src='bin', dst='bin')
-        self.copy('*.dylib', dst='lib', src='lib')
-        self.copy('*.so',    dst='lib', src='lib')
+        self.copy('*', src='bin', dst='bin')
+        self.copy('*', dst='lib', src='lib')
 
     def test(self):
         self.run('qbs run')
 
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
-            self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/libzmq.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -l lib/libzmq.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
-            self.run('! (ldd lib/*.so | grep -v "^lib/" | grep "/" | egrep -v "\s/lib64/")')
+            self.run('! (ldd lib/libzmq.so | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
